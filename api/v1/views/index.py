bda01ffa4b3c62de3f829b48e06b4b56f6bd322f
crasride <jose.dfernandez@yahoo.fr>
2022-12-06 08:09:20 +0100

    fix pycode

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,9 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 # create routes for objects taht return json status
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ status code """
     return jsonify({'status': 'OK'})
-
