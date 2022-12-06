b5710706bffdcf332e2cb1885b0be93b0ba6341f
crasride <jose.dfernandez@yahoo.fr>
2022-12-06 17:17:56 +0100

    7. State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -25,7 +25,8 @@ def get_state(state_id=None):
 
 
 # Deletes a State object:: DELETE /api/v1/states/<state_id>
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """deletes a state based on id"""
     state = storage.get(State, state_id)
