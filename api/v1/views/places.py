e6148ae405361fb14a75df9e25fa0974ddabe94a
crasride <jose.dfernandez@yahoo.fr>
2022-12-07 17:51:31 +0100

    places modifi post

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,7 @@ from flask import abort, request
 from models import storage
 from models.place import Place
 from models.city import City
+from models.state import State
 
 
 # Retrieves the list of all Place objects of a City
@@ -44,29 +45,30 @@ def delete_place(place_id):
 # Creates a Place: POST /api/v1/cities/<city_id>/places
 @app_views.route('/cities/<city_id>/places/', methods=['POST'],
                  strict_slashes=False)
-def create_place_city_id(city_id=None):
+def create_place_city_id(city_id):
     """ Creates Place """
-    city = storage.get(City, city_id)
-    if city is None:
-        abort(404)
-    opc_reqst = request.get_json()
-    if opc_reqst is None:
-        return 'Not a JSON', 400
-    if 'user_id' not in opc_reqst:
-        return 'Missing user_id', 400
-    user = storage.get("User", opc_reqst.get("user_id"))
-    if user is None:
-        abort(404)
-    if 'name' not in opc_reqst:
-        return 'Missing name', 400
-    opc_reqst['city_id'] = city_id
-    n_place = Place(**opc_reqst)
-    n_place.save()
-    return jsonify(n_place.to_dict()), 201
+    if request.method == 'POST':
+        city = storage.get(City, city_id)
+        if city is None:
+            abort(404)
+        opc_reqst = request.get_json()
+        if opc_reqst is None:
+            return 'Not a JSON', 400
+        if 'user_id' not in opc_reqst:
+            return 'Missing user_id', 400
+        user = storage.get("User", opc_reqst.get("user_id"))
+        if user is None:
+            abort(404)
+        if 'name' not in opc_reqst:
+            return 'Missing name', 400
+        opc_reqst['city_id'] = city_id
+        n_place = Place(**opc_reqst)
+        n_place.save()
+        return jsonify(n_place.to_dict()), 201
 
 # Updates a Place object: PUT /api/v1/places/<place_id>
 @app_views.route('/places/<place_id>/', methods=['PUT'], strict_slashes=False)
-def update_place(place_id=None):
+def update_place(place_id):
     """ Update Place Object """
     n_dict = storage.get(Place, place_id)
     if n_dict is None:
